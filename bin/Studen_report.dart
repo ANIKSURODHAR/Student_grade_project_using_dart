9b71ef7f3422135523adc7263cff6bf90badec5b
Anik Sutrodhar <aniksutrodhar@Aniks-MacBook-Pro.local>
2026-04-18 22:49:50 +0600

    add option2

--- bin/Studen_report.dart
+++ bin/Studen_report.dart
@@ -1,23 +1,49 @@
 import 'dart:io';
 
-List<Map<String, dynamic>>Students = [];
+List<Map<String,dynamic>>Students = [];
 final Set<String>availableSubjects={"Math","English","Science","History"};
 
+void Add_Bonus_Points(){
+  if(Students[i])
+}
+void Record_Score(){
 
+  for(int i=0;i<Students.length;i++){
+    print("${i+1}. ${Students[i]['name']}");
+  }
+  stdout.write("choose a  student");
+  var chose=int.parse(stdin.readLineSync()!);
+  if(chose<1 && chose>Students.length) print("you enter wrong student");
+  else{
+    int score=-1;
+    while(score<0 || score>100) {
+      print("Available Subject ${Students[chose-1]['subjects'].join(', ')}");
+      stdout.write("Enter number between 1-100");
+      score=int.parse(stdin.readLineSync() ?? "")??-1;
+      if(score<0 || score>100){
+        stdout.write("Invalid input. Score must be exactly between 0 and 100:");
+      }
+    }
 
+    Students[chose-1]['scores'].add(score);
+    print("The $score is added for in  ${Students[chose-1]['name'] }");
+
+  }
+}
 void Add(){
  stdout.write("Enter Student name");
  String name=stdin.readLineSync()??'Unknown';
  var newStudent=<String,dynamic>{
    'name': name,
    'scores':<int>[],
-   'subject':{...availableSubjects},
+   'subjects':{...availableSubjects},
    'bonus':null,
    'comment':null
  };
  Students.add(newStudent);
 
  print("\nSuccessfully $name added in the Student lists");
+
 }
 
 void main() {
@@ -40,6 +66,17 @@ void main() {
     switch (decision) {
       case 1:
         Add();
+        break;
+      case 2:
+        Record_Score();
+        break;
+      case 3:
+        Add_Bonus_Points();
+        break;
+      case 4:
+      case 5:
+      case 6:
+      case 7:
       case 8:
         exit = false;
     }
---
+++ bin/student.dart
@@ -0,0 +1,7 @@
+var Students=<String,dynamic>{
+  "name":name,
+  "scores":<int>[],
+  "subjects":{...availableSubjects},
+  "bonus":null,
+  "comment":null
+}
